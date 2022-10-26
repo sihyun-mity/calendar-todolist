61ee859d38da466e270215904d0554ec86edefab
Sihyeon Hong <kailker4467@gmail.com>
2022-10-26 20:37:57 +0900

    date-fns 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import { date } from './stores/atom';
+import { format } from 'date-fns';
+import { ko } from 'date-fns/locale';
+
 const App = (): JSX.Element => {
+  const [now, setNow] = useRecoilState(date);
+  console.log(now);
+  console.log(format(now, 'PPP', { locale: ko }));
+
   return <div></div>;
 };
 
